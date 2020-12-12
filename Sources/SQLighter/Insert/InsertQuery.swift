f088fb5671326e0075bf6eed83c239cc80a40f00
Aleksey Zgurskiy <mr.noone@icloud.com>
2020-12-12 15:56:51 +0200

    Add array columns to insert statement

--- Sources/SQLighter/Insert/InsertQuery.swift
+++ Sources/SQLighter/Insert/InsertQuery.swift
@@ -2,4 +2,5 @@ import Foundation
 
 public protocol InsertQuery: InsertValue {
   func column<C: SQLColumn>(_ names: C...) -> InsertQuery
+  func column<C: SQLColumn>(_ names: [C]) -> InsertQuery
 }
