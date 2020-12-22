9a7a12e3440053d4dc302d172f470c324423b889
Aleksey Zgurskiy <mr.noone@icloud.com>
2020-12-22 15:11:18 +0200

    Add expression predicate

--- Sources/SQLighter/Core/Predicates/ComparisonPredicate.swift
+++ Sources/SQLighter/Core/Predicates/ComparisonPredicate.swift
@@ -19,37 +19,73 @@ struct ComparisonPredicate: Predicate {
   let `operator`: Operator
   let column: SQLColumn
   let value: SQLValueConvertible?
+  let expression: SQLConvertible?
   
   // MARK: - SQLConvertible
   
   func sqlQuery() -> SQLQuery {
-    let sql = "\(column.sqlString) \(`operator`.sqlString) ?"
-    return .init(sql: sql, args: [value])
+    let rightOperand: String
+    let arguments: Arguments
+    
+    if let query = expression?.sqlQuery() {
+      rightOperand = "(\(query.sql))"
+      arguments = query.args
+    } else {
+      rightOperand = "?"
+      arguments = [value]
+    }
+    
+    let sql = "\(column.sqlString) \(`operator`.sqlString) \(rightOperand)"
+    return .init(sql: sql, args: arguments)
   }
 }
 
 // MARK: - Operators
 
 public func == (lhs: SQLColumn, rhs: SQLValueConvertible?) -> Predicate {
-  return ComparisonPredicate(operator: .equal, column: lhs, value: rhs)
+  return ComparisonPredicate(operator: .equal, column: lhs, value: rhs, expression: nil)
+}
+
+public func == (lhs: SQLColumn, rhs: SQLConvertible) -> Predicate {
+  return ComparisonPredicate(operator: .equal, column: lhs, value: nil, expression: rhs)
 }
 
 public func != (lhs: SQLColumn, rhs: SQLValueConvertible?) -> Predicate {
-  return ComparisonPredicate(operator: .notEqual, column: lhs, value: rhs)
+  return ComparisonPredicate(operator: .notEqual, column: lhs, value: rhs, expression: nil)
+}
+
+public func != (lhs: SQLColumn, rhs: SQLConvertible) -> Predicate {
+  return ComparisonPredicate(operator: .notEqual, column: lhs, value: nil, expression: rhs)
 }
 
 public func < (lhs: SQLColumn, rhs: SQLValueConvertible?) -> Predicate {
-  return ComparisonPredicate(operator: .lessThan, column: lhs, value: rhs)
+  return ComparisonPredicate(operator: .lessThan, column: lhs, value: rhs, expression: nil)
+}
+
+public func < (lhs: SQLColumn, rhs: SQLConvertible) -> Predicate {
+  return ComparisonPredicate(operator: .lessThan, column: lhs, value: nil, expression: rhs)
 }
 
 public func > (lhs: SQLColumn, rhs: SQLValueConvertible?) -> Predicate {
-  return ComparisonPredicate(operator: .greaterThan, column: lhs, value: rhs)
+  return ComparisonPredicate(operator: .greaterThan, column: lhs, value: rhs, expression: nil)
+}
+
+public func > (lhs: SQLColumn, rhs: SQLConvertible) -> Predicate {
+  return ComparisonPredicate(operator: .greaterThan, column: lhs, value: nil, expression: rhs)
 }
 
 public func <= (lhs: SQLColumn, rhs: SQLValueConvertible?) -> Predicate {
-  return ComparisonPredicate(operator: .lessThanOrEqual, column: lhs, value: rhs)
+  return ComparisonPredicate(operator: .lessThanOrEqual, column: lhs, value: rhs, expression: nil)
+}
+
+public func <= (lhs: SQLColumn, rhs: SQLConvertible) -> Predicate {
+  return ComparisonPredicate(operator: .lessThanOrEqual, column: lhs, value: nil, expression: rhs)
 }
 
 public func >= (lhs: SQLColumn, rhs: SQLValueConvertible?) -> Predicate {
-  return ComparisonPredicate(operator: .greaterThanOrEqual, column: lhs, value: rhs)
+  return ComparisonPredicate(operator: .greaterThanOrEqual, column: lhs, value: rhs, expression: nil)
+}
+
+public func >= (lhs: SQLColumn, rhs: SQLConvertible) -> Predicate {
+  return ComparisonPredicate(operator: .greaterThanOrEqual, column: lhs, value: nil, expression: rhs)
 }
--- Tests/SQLighterTests/ComparisonPredicateTests.swift
+++ Tests/SQLighterTests/ComparisonPredicateTests.swift
@@ -8,10 +8,22 @@ final class ComparisonPredicateTests: XCTestCase {
     XCTAssertEqual(predicate.sqlString(), "id = 123")
   }
   
+  func testExpressionEqualPredicate() {
+    let predicate = "id" == SQL.select(column: "test_id").from(table: "User").limit(1)
+    XCTAssertEqual(predicate.sqlQuery().sql, "id = (SELECT test_id FROM User LIMIT 1)")
+    XCTAssertEqual(predicate.sqlString(), "id = (SELECT test_id FROM User LIMIT 1)")
+  }
+  
   func testNotEqualPredicate() {
     let predicate: Predicate = "id" != "str"
     XCTAssertEqual(predicate.sqlQuery().sql, "id != ?")
     XCTAssertEqual(predicate.sqlString(), "id != 'str'")
+  }
+  
+  func testExpressionNotEqualPredicate() {
+    let predicate = "id" != SQL.select(column: "test_id").from(table: "User").limit(1)
+    XCTAssertEqual(predicate.sqlQuery().sql, "id != (SELECT test_id FROM User LIMIT 1)")
+    XCTAssertEqual(predicate.sqlString(), "id != (SELECT test_id FROM User LIMIT 1)")
   }
   
   func testLessThanPredicate() {
@@ -20,10 +32,22 @@ final class ComparisonPredicateTests: XCTestCase {
     XCTAssertEqual(predicate.sqlString(), "id < 11")
   }
   
+  func testExpressionLessThanPredicate() {
+    let predicate = "id" < SQL.select(column: "test_id").from(table: "User").limit(1)
+    XCTAssertEqual(predicate.sqlQuery().sql, "id < (SELECT test_id FROM User LIMIT 1)")
+    XCTAssertEqual(predicate.sqlString(), "id < (SELECT test_id FROM User LIMIT 1)")
+  }
+  
   func testGreaterThanPredicate() {
     let predicate: Predicate = "id" > "abc"
     XCTAssertEqual(predicate.sqlQuery().sql, "id > ?")
     XCTAssertEqual(predicate.sqlString(), "id > 'abc'")
+  }
+  
+  func testExpressionGreaterThanPredicate() {
+    let predicate = "id" > SQL.select(column: "test_id").from(table: "User").limit(1)
+    XCTAssertEqual(predicate.sqlQuery().sql, "id > (SELECT test_id FROM User LIMIT 1)")
+    XCTAssertEqual(predicate.sqlString(), "id > (SELECT test_id FROM User LIMIT 1)")
   }
   
   func testLessThanOrEqualPredicate() {
@@ -32,9 +56,21 @@ final class ComparisonPredicateTests: XCTestCase {
     XCTAssertEqual(predicate.sqlString(), "id <= NULL")
   }
   
+  func testExpressionLessThanOrEqualPredicate() {
+    let predicate = "id" <= SQL.select(column: "test_id").from(table: "User").limit(1)
+    XCTAssertEqual(predicate.sqlQuery().sql, "id <= (SELECT test_id FROM User LIMIT 1)")
+    XCTAssertEqual(predicate.sqlString(), "id <= (SELECT test_id FROM User LIMIT 1)")
+  }
+  
   func testGreaterThanOrEqualPredicate() {
     let predicate = "id" >= 123
     XCTAssertEqual(predicate.sqlQuery().sql, "id >= ?")
     XCTAssertEqual(predicate.sqlString(), "id >= 123")
+  }
+  
+  func testExpressionGreaterThanOrEqualPredicate() {
+    let predicate = "id" >= SQL.select(column: "test_id").from(table: "User").limit(1)
+    XCTAssertEqual(predicate.sqlQuery().sql, "id >= (SELECT test_id FROM User LIMIT 1)")
+    XCTAssertEqual(predicate.sqlString(), "id >= (SELECT test_id FROM User LIMIT 1)")
   }
 }
